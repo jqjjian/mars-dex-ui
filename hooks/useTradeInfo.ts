295c5a300f2d7abce5cefe6643f84551205e4437
jqjjian <jqjjian13@gmail.com>
2024-09-27 16:53:07 +0800

    fix price

--- hooks/useTradeInfo.ts
+++ hooks/useTradeInfo.ts
@@ -1,29 +1,13 @@
-import { useMemo, useCallback, useState, useEffect } from 'react'
+import { useMemo, useCallback, useEffect } from 'react'
 import * as R from 'ramda'
-import {
-    useAccount,
-    useReadContract,
-    useReadContracts,
-    useSendTransaction,
-    // usePrepareTransactionRequest,
-    // useConnectors,
-    useBlockNumber,
-    useBalance,
-    // useConnect,
-    type UseAccountParameters,
-    type UseReadContractsReturnType
-} from 'wagmi'
-import {
-    type Address,
-    type MulticallContracts,
-    formatUnits,
-    parseUnits
-} from 'viem'
-import { MonoTradeAbi, ERC20Abi, TradeServiceAbi } from '@/constants/abi'
+import { useAccount, useReadContracts, useBlockNumber } from 'wagmi'
+import { type Address, formatUnits } from 'viem'
+import { TradeServiceAbi } from '@/constants/abi'
 import { config } from '@/config'
-import { type TokenInfoType, useMarsDexStore } from '@/lib/store'
+import { useMarsDexStore } from '@/lib/store'
 import { uint32Value } from '@/constants/data'
 import { useTokenInfoByTrade } from '@/hooks'
+
 type OrderListType = {
     amountIn: bigint
     amountOut: bigint
@@ -32,25 +16,21 @@ type OrderListType = {
 }
 
 export default function useTradeInfo(tradeAddr: Address) {
-    const [tradeList, setTradeList] = useState([])
     const {
         serviceAddr,
         sellMemeTradeAddr,
         buyMemeTradeAddr,
-        setBuyMemeTradeAddr,
         setSellMemeTradeAddr,
+        setBuyMemeTradeAddr,
         setSellList,
         setBuyList,
         setCurrentPrice,
-        // currentPrice,
         setToken0Info,
         setToken1Info,
         setCurrentTradeMode
     } = useMarsDexStore()
-    const { data: blockNumber, refetch: refetchBigNum } = useBlockNumber()
-    const { address, isConnected, chainId } = useAccount({
-        ...config
-    } as UseAccountParameters)
+    const { data: blockNumber, refetch: refetchData } = useBlockNumber()
+    // const { address, isConnected, chainId } = useAccount()
 
     const { token0Info, token1Info, tradeTokens } =
         useTokenInfoByTrade(tradeAddr)
@@ -58,184 +38,134 @@ export default function useTradeInfo(tradeAddr: Address) {
     const [usdtInfo, memeInfo, usdtAddr, memeAddr] = useMemo(() => {
         if (!token0Info || !token1Info || !tradeTokens)
             return [null, null, null, null]
-
         const isToken0USDT = token0Info.symbol === 'USDT'
         return isToken0USDT
             ? [token0Info, token1Info, tradeTokens[0], tradeTokens[1]]
             : [token1Info, token0Info, tradeTokens[1], tradeTokens[0]]
     }, [token0Info, token1Info, tradeTokens])
 
-    const serviceArgs = {
-        abi: TradeServiceAbi,
-        address: serviceAddr,
-        functionName: 'getTrade'
-    }
-
     const { data: _tradeAddrs } = useReadContracts({
         contracts: [
             {
-                ...serviceArgs,
+                abi: TradeServiceAbi,
+                address: serviceAddr,
+                functionName: 'getTrade',
                 args: [usdtAddr, memeAddr]
             },
             {
-                ...serviceArgs,
+                abi: TradeServiceAbi,
+                address: serviceAddr,
+                functionName: 'getTrade',
                 args: [memeAddr, usdtAddr]
             }
         ]
     })
-    const tradeAddrs: Address[] = useMemo(() => {
-        return (
-            (_tradeAddrs &&
-                R.map<{ result: Address; status: string }, Address>(
-                    R.prop<'result'>('result')
-                )([..._tradeAddrs] as {
-                    result: Address
-                    status: string
-                }[])) ||
-            []
-        )
-    }, [_tradeAddrs])
 
-    let fromOrderId = 0
-    let num = 30
-    const tradeListArgs = {
-        address: serviceAddr,
-        abi: TradeServiceAbi,
-        functionName: 'getOrderList'
-    } as const
+    const tradeAddrs: Address[] = useMemo(
+        () => _tradeAddrs?.map(({ result }) => result as Address) || [],
+        [_tradeAddrs]
+    )
+
     const { data: _tradeList } = useReadContracts({
         blockNumber,
         contracts: [
             {
-                ...tradeListArgs,
-                args: [sellMemeTradeAddr, fromOrderId, num]
+                address: serviceAddr,
+                abi: TradeServiceAbi,
+                functionName: 'getOrderList',
+                args: [sellMemeTradeAddr, 0, 30]
             },
             {
-                ...tradeListArgs,
-                args: [buyMemeTradeAddr, fromOrderId, num]
+                address: serviceAddr,
+                abi: TradeServiceAbi,
+                functionName: 'getOrderList',
+                args: [buyMemeTradeAddr, 0, 30]
             }
         ]
     })
-    // console.log({ _tradeList })
-    const sellList = useMemo(() => {
-        const list =
-            (_tradeList && (_tradeList[0].result as OrderListType[])) || []
-        const priceList = list
-            .filter((v) => v.orderId !== 0)
-            .reverse()
-            .map((v) => {
-                const _out = formatUnits(v.amountOut, usdtInfo!.decimals)
-                // console.log(_out)
-                // console.log(token0Info?.decimals)
-                const _in = formatUnits(v.amountIn, memeInfo!.decimals)
-                const _amount = formatUnits(v.amountIn, memeInfo!.decimals)
-                const proportion =
-                    v.progress !== 0 ? 1 - v.progress / uint32Value : 1
 
-                return {
-                    price: +(+_out / +_in).toFixed(4),
-                    amount: proportion * +_amount
-                }
-                // console.log(_in)
-                // console.log(token1Info?.decimals)
-                // return +_out / +_in
-            })
-        const _priceList = R.map<{ price: number; amount: number }, number>(
-            R.prop<'price'>('price')
-        )(priceList)
-        const uniqPriceList = R.uniq(_priceList)
-        // console.log({ uniqPriceList })
-        // console.log(uniqPriceList)
-        return uniqPriceList.map((price) => {
-            const filterList = priceList.filter((v) => price === v.price)
-            const amount = R.sum(
-                R.map<{ price: number; amount: number }, number>(
-                    R.prop<'amount'>('amount')
-                )(filterList)
-            )
-            return {
+    const processOrderList = useCallback(
+        (list: OrderListType[], isUSDT: boolean) => {
+            return list
+                .filter((v) => v.orderId !== 0)
+                .map((v) => {
+                    const _out = +formatUnits(
+                        v.amountOut,
+                        isUSDT ? usdtInfo!.decimals : memeInfo!.decimals
+                    )
+                    const _in = +formatUnits(
+                        v.amountIn,
+                        isUSDT ? memeInfo!.decimals : usdtInfo!.decimals
+                    )
+                    const _amount = +formatUnits(
+                        isUSDT ? v.amountIn : v.amountOut,
+                        memeInfo!.decimals
+                    )
+                    const proportion =
+                        v.progress !== 0 ? 1 - v.progress / uint32Value : 1
+                    return {
+                        price: isUSDT
+                            ? +(_out / _in).toFixed(4)
+                            : +(_in / _out).toFixed(4),
+                        amount: proportion * _amount
+                    }
+                })
+        },
+        [usdtInfo, memeInfo]
+    )
+
+    const aggregateOrders = useCallback(
+        (priceList: { price: number; amount: number }[]) => {
+            const uniqPrices = R.uniq(priceList.map(R.prop('price')))
+            return uniqPrices.map((price) => ({
                 price: +price.toFixed(4),
-                amount: +amount.toFixed(4)
-            }
-        })
-    }, [_tradeList, usdtInfo, memeInfo])
+                amount: +R.sum(
+                    priceList
+                        .filter((v) => v.price === price)
+                        .map(R.prop('amount'))
+                ).toFixed(4)
+            }))
+        },
+        []
+    )
+
+    const sellList = useMemo(() => {
+        const list = (_tradeList?.[0].result as OrderListType[]) || []
+        return aggregateOrders(processOrderList(list, true)).reverse()
+    }, [_tradeList, processOrderList, aggregateOrders])
+
     const buyList = useMemo(() => {
-        const list =
-            (_tradeList && (_tradeList[1].result as OrderListType[])) || []
-        const priceList = list
-            .filter((v) => v.orderId !== 0)
-            .map((v) => {
-                const _out = +formatUnits(v.amountOut, memeInfo!.decimals)
-                const _in = +formatUnits(v.amountIn, usdtInfo!.decimals)
-                const _amount = formatUnits(v.amountOut, memeInfo!.decimals)
-                const proportion =
-                    v.progress !== 0 ? 1 - v.progress / uint32Value : 1
-                // console.log(proportion)
-                return {
-                    price: +(_in / _out).toFixed(4),
-                    amount: proportion * +_amount
-                }
-                // return (
-                //     +formatUnits(v.amountIn, token0Info?.decimals || 6) /
-                //     +formatUnits(v.amountOut, token1Info?.decimals || 18)
-                // )
-            })
-        const _priceList = R.map<{ price: number; amount: number }, number>(
-            R.prop<'price'>('price')
-        )(priceList)
-        const uniqPriceList = R.uniq(_priceList)
-        // console.log(uniqPriceList)
-        return uniqPriceList.map((price) => {
-            // console.log(price)
-            const filterList = priceList.filter((v) => price === v.price)
-            const amount = R.sum(
-                R.map<{ price: number; amount: number }, number>(
-                    R.prop<'amount'>('amount')
-                )(filterList)
-            )
-            return {
-                price: +price.toFixed(4),
-                amount: +amount.toFixed(4)
-            }
-        })
-    }, [_tradeList, usdtInfo, memeInfo])
-    // console.log(sellList, buyList)
+        const list = (_tradeList?.[1].result as OrderListType[]) || []
+        return aggregateOrders(processOrderList(list, false))
+    }, [_tradeList, processOrderList, aggregateOrders])
+
     useEffect(() => {
-        if (tradeAddrs && tradeAddrs.length === 2) {
+        if (tradeAddrs.length === 2) {
             setSellMemeTradeAddr(tradeAddrs[0])
             setBuyMemeTradeAddr(tradeAddrs[1])
         }
-    }, [tradeAddrs])
+    }, [tradeAddrs, setSellMemeTradeAddr, setBuyMemeTradeAddr])
+
     useEffect(() => {
-        sellList && setSellList(sellList)
-    }, [sellList])
-    useEffect(() => {
-        buyList && setBuyList(buyList)
-    }, [buyList])
-    useEffect(() => {
-        if (sellList && buyList) {
-            const _price =
-                (sellList[sellList.length - 1]?.price ??
-                    0 + buyList[0]?.price ??
-                    0) / 2
-            setCurrentPrice(_price)
-        }
-    }, [sellList, buyList])
+        setSellList(sellList)
+        setBuyList(buyList)
+        const _price =
+            ((sellList[sellList.length - 1]?.price ?? 0) +
+                (buyList[0]?.price ?? 0)) /
+            2
+        setCurrentPrice(Math.round(_price * 10000) / 10000)
+    }, [sellList, buyList, setSellList, setBuyList, setCurrentPrice])
 
     useEffect(() => {
         if (token0Info) setToken0Info(token0Info)
         if (token1Info) setToken1Info(token1Info)
-    }, [token0Info, token1Info])
+    }, [token0Info, token1Info, setToken0Info, setToken1Info])
 
     useEffect(() => {
-        if (tradeAddr && tradeAddrs) {
+        if (tradeAddr && tradeAddrs.length) {
             setCurrentTradeMode(tradeAddr === tradeAddrs[0] ? 'SELL' : 'BUY')
         }
-    }, [tradeAddr, tradeAddrs])
-
-    const refetchData = useCallback(() => {
-        refetchBigNum()
-    }, [refetchBigNum])
+    }, [tradeAddr, tradeAddrs, setCurrentTradeMode])
 
     return {
         sellList,
